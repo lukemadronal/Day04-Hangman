77f741fe7cabab875ab3031d1e4baf350b8b3a0f
Luke Madronal <madronal@lafayette.edu>
2015-09-24 14:04:12 -0400

    added random word feature. plus all of the words from the dictionary file are in an array.

--- Hangman/Hangman/ViewController.m
+++ Hangman/Hangman/ViewController.m
@@ -10,12 +10,43 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong)NSArray *wordList;
+
 @end
 
 @implementation ViewController
 
+NSString *temp=@"";
+
+- (NSString *)readBundleFileToString:(NSString *)filename ofType: (NSString *)type {
+    NSString *path = [[NSBundle mainBundle] pathForResource:filename ofType:type];
+   // NSLog(@"got here");
+    return [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:NULL];
+}
+
+- (NSArray *)convertCSVStringToArray:(NSString *)csvString {
+    NSString *cleanString = [[csvString componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]] componentsJoinedByString:@""];
+    NSCharacterSet *set = [NSCharacterSet characterSetWithCharactersInString:@","];
+    //NSLog(@"got into second");
+    return [cleanString componentsSeparatedByCharactersInSet:set];
+}
+
+- (IBAction)startNewGamePressed:(id)sender {
+    NSLog(@"%@",[self randomWord:(_wordList)]);
+
+}
+
+- (NSString *) randomWord:(NSArray *)wordList {
+    return _wordList [arc4random_uniform((uint32_t)[_wordList count]-1)];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"view did load");
+    _wordList=[self convertCSVStringToArray: [self readBundleFileToString:@"WordSetApple" ofType: @"csv"]];
+    
+    
+    //[self readBundleFileToString:temp WordSetApple ofType :temp];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
