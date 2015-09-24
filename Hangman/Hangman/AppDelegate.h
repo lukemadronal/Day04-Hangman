ccc124edabbd17ec84cd876584cb2defcefe13f0
Luke Madronal <madronal@lafayette.edu>
2015-09-24 12:00:16 -0400

    initial commit.

---
+++ Hangman/Hangman/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hangman
+//
+//  Created by Luke Madronal on 9/24/15.
+//  Copyright © 2015 Luke Madronal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hangman/Hangman/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hangman
+//
+//  Created by Luke Madronal on 9/24/15.
+//  Copyright © 2015 Luke Madronal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
