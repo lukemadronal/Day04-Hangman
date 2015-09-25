437e7bd2871fa0b3ed23198384315bb30c91bfa5
Luke Madronal <madronal@lafayette.edu>
2015-09-25 14:52:46 -0400

    everything but win condition and layout

--- Hangman/Hangman/ViewController.m
+++ Hangman/Hangman/ViewController.m
@@ -26,10 +26,11 @@
 
 
 int lifeCount=0;
+bool freshgame;
 
 - (NSString *)readBundleFileToString:(NSString *)filename ofType: (NSString *)type {
     NSString *path = [[NSBundle mainBundle] pathForResource:filename ofType:type];
-   // NSLog(@"got here");
+    // NSLog(@"got here");
     return [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:NULL];
 }
 
@@ -41,46 +42,12 @@ int lifeCount=0;
 }
 
 - (void)test{
-lifeCount=0;
-             _newgamedisplay.titleLabel.text = @"GUESS LETTERS!";
-             _guessWord=[self randomWord:(_wordList)];
-             _letterList= [[NSMutableArray alloc] init];
-             for (int i=0;i<_guessWord.length;i++) {
-                 //        NSString *letter = [_guessWord characterAtIndex:i];
-                 NSString *letter = [_guessWord substringWithRange:NSMakeRange(i, 1)];
-                 [_letterList addObject:letter];
-             }
-             [_excludedButtons removeAllObjects];
-             //clear death counter
-             
-             for (id object in [_firstView subviews]) {
-                 [object removeFromSuperview];
-             }
-             
-             for (int i=0;i<_guessWord.length;i++) {
-                 UIView *hashMark = [[UIView alloc] initWithFrame:CGRectMake(10.0+(20*i), 23.0, 10, 2)];
-                 [hashMark setBackgroundColor:[UIColor grayColor]];
-                 [_firstView addSubview:hashMark];
-                 UILabel *letterLabel = [[UILabel alloc] initWithFrame:CGRectMake(10.0+(20*i), 0.0, 10, 21)];
-                 letterLabel.textColor = [UIColor whiteColor];
-                 letterLabel.font = [UIFont fontWithName:@"Futura" size:16.0];
-                 letterLabel.tag = i;
-                 letterLabel.text = _letterList[i];
-                 [_firstView addSubview:letterLabel];
-                 
-                 
-             }
-             
-             }
-
-
-- (IBAction)startNewGamePressed:(id)sender {
     lifeCount=0;
     _newgamedisplay.titleLabel.text = @"GUESS LETTERS!";
     _guessWord=[self randomWord:(_wordList)];
-     _letterList= [[NSMutableArray alloc] init];
-        for (int i=0;i<_guessWord.length;i++) {
-//        NSString *letter = [_guessWord characterAtIndex:i];
+    _letterList= [[NSMutableArray alloc] init];
+    for (int i=0;i<_guessWord.length;i++) {
+        //        NSString *letter = [_guessWord characterAtIndex:i];
         NSString *letter = [_guessWord substringWithRange:NSMakeRange(i, 1)];
         [_letterList addObject:letter];
     }
@@ -90,7 +57,7 @@ lifeCount=0;
     for (id object in [_firstView subviews]) {
         [object removeFromSuperview];
     }
-
+    
     for (int i=0;i<_guessWord.length;i++) {
         UIView *hashMark = [[UIView alloc] initWithFrame:CGRectMake(10.0+(20*i), 23.0, 10, 2)];
         [hashMark setBackgroundColor:[UIColor grayColor]];
@@ -101,57 +68,96 @@ lifeCount=0;
         letterLabel.tag = i;
         letterLabel.text = _letterList[i];
         [_firstView addSubview:letterLabel];
-
+        
         
     }
+    
+}
 
+
+- (IBAction)startNewGamePressed:(id)sender {
+    freshgame=true;
+    lifeCount=0;
+    _guessWord=[self randomWord:(_wordList)];
+    _letterList= [[NSMutableArray alloc] init];
+    for (int i=0;i<_guessWord.length;i++) {
+        //        NSString *letter = [_guessWord characterAtIndex:i];
+        NSString *letter = [_guessWord substringWithRange:NSMakeRange(i, 1)];
+        [_letterList addObject:letter];
+    }
+    [_excludedButtons removeAllObjects];
+    //clear death counter
+    
+    for (id object in [_firstView subviews]) {
+        [object removeFromSuperview];
+    }
+    
+    for (int i=0;i<_guessWord.length;i++) {
+        UIView *hashMark = [[UIView alloc] initWithFrame:CGRectMake(10.0+(20*i), 23.0, 10, 2)];
+        [hashMark setBackgroundColor:[UIColor grayColor]];
+        [_firstView addSubview:hashMark];
+        UILabel *letterLabel = [[UILabel alloc] initWithFrame:CGRectMake(10.0+(20*i), 0.0, 10, 21)];
+        letterLabel.textColor = [UIColor whiteColor];
+        letterLabel.font = [UIFont fontWithName:@"Futura" size:16.0];
+        letterLabel.tag = i;
+        letterLabel.text = _letterList[i];
+        [_firstView addSubview:letterLabel];
+        
+        
+    }
+    
 }
 
 - (IBAction)keyboardPressed:(UIButton *)button {
-    NSString *buttonLetter = [button currentTitle];
-    NSLog(@"%@ Letter Pressed", buttonLetter );
-    NSString *buttontemp =[NSString stringWithFormat:@"%@", buttonLetter];
-    if (![_excludedButtons containsObject:(@"a")]) {
-        NSLog(@"%@", buttontemp);
-        [self checkLetter:buttonLetter];
+    if(freshgame){
+        [button setEnabled:NO];
+        NSString *buttonLetter = [button currentTitle];
+        NSLog(@"%@ Letter Pressed", buttonLetter );
+        NSString *buttontemp =[NSString stringWithFormat:@"%@", buttonLetter];
+        if (![_excludedButtons containsObject:(@"a")]) {
+            NSLog(@"%@", buttontemp);
+            [self checkLetter:buttonLetter];
+        }
     }
-    
 }
-    
+
 
 -(void)checkLetter:(NSString *) letter {
-    [_excludedButtons addObject:letter];
-    NSLog(@"the letter is: %@ and the word is: %@",letter,_guessWord);
-    if ([[_guessWord uppercaseString] containsString:letter]) {
-        NSLog(@"i found an A");
-        //display letter
-        for (int i=0; i < _letterList.count; i++) {
-            if( [[_letterList[i] uppercaseString] isEqualToString:letter] ) {
-                NSLog(@"the letter is: %@ and the word is: %@",letter,_guessWord);
-                for (id object in [_firstView subviews]) {
-                    if ([object isKindOfClass: [UILabel class]]) {
-                        UILabel *currentLabel = (UILabel *)object;
-                        if (currentLabel.tag == i) {
-                            currentLabel.textColor = [UIColor blackColor];
+    if (freshgame) {
+        [_excludedButtons addObject:letter];
+        NSLog(@"the letter is: %@ and the word is: %@",letter,_guessWord);
+        if ([[_guessWord uppercaseString] containsString:letter]) {
+            NSLog(@"i found an A");
+            //display letter
+            for (int i=0; i < _letterList.count; i++) {
+                if( [[_letterList[i] uppercaseString] isEqualToString:letter] ) {
+                    NSLog(@"the letter is: %@ and the word is: %@",letter,_guessWord);
+                    for (id object in [_firstView subviews]) {
+                        if ([object isKindOfClass: [UILabel class]]) {
+                            UILabel *currentLabel = (UILabel *)object;
+                            if (currentLabel.tag == i) {
+                                currentLabel.textColor = [UIColor blackColor];
+                            }
                         }
                     }
                 }
             }
-        }
-        
-    } else {
-        lifeCount++;
-        NSString *temp=[NSString stringWithFormat:@"Hangman%i.png", lifeCount];
-        [_nooseview setImage:[UIImage imageNamed:temp]];
-        if (lifeCount == 10) {UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Oh No!" message:@"You LOSE" preferredStyle:UIAlertControllerStyleAlert];
-            UIAlertAction *defaultAction = [UIAlertAction actionWithTitle:@"Start New Game" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
-                [self test];
-                                                }];
+            
+        } else {
+            lifeCount++;
+            NSString *temp=[NSString stringWithFormat:@"Hangman%i.png", lifeCount];
+            [_nooseview setImage:[UIImage imageNamed:temp]];
+            if (lifeCount == 10) {UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Oh No!" message:@"You LOSE" preferredStyle:UIAlertControllerStyleAlert];
+                UIAlertAction *defaultAction = [UIAlertAction actionWithTitle:@"Start New Game" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+                    [self test];
+                }];
                 [alert addAction:defaultAction];
                 [self presentViewController:alert animated:YES completion:nil];
-        
+                
+            }
         }
     }
+    
 }
 
 - (NSString *) randomWord:(NSArray *)wordList {
@@ -161,6 +167,7 @@ lifeCount=0;
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"view did load");
+    freshgame=false;
     _wordList=[self convertCSVStringToArray: [self readBundleFileToString:@"WordSetApple" ofType: @"csv"]];
     
     
